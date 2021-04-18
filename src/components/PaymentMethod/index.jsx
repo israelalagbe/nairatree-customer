e4d0dd47df91b02fca6fd8d1e8966c12454422c3
Israel Alagbe <israelalagbe53@gmail.com>
2021-04-18 23:36:25 +0100

    Checkout updates

--- src/components/PaymentMethod/index.jsx
+++ src/components/PaymentMethod/index.jsx
@@ -5,34 +5,76 @@ import lock from "../../img/lock.png";
 import "./index.scss";
 
 function PaymentMethod() {
+  const [formData, setFormData] = React.useState({
+    card_number: "",
+    exp_date: "",
+    cvv: "",
+  });
+
+  const [paymentMode, setPaymentMode] = React.useState(null);
+
+  const handleChange = async (e) => {
+    if(e.target.name ==='card_number' && !Number(e.target.value) && e.target.value!== '') return;
+
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(paymentMode)
+  };
+
   return (
     <div className="payment-method">
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <div className="payment-form">
-          <div className='input-container'>
+          <div className="input-container">
             <FormGroup>
               <Label for="fullName">Card Number</Label>
               <Input
+                onChange={handleChange}
+                minLength={15}
+                maxLength={16}
                 type="text"
-                name="cardNumber"
-                id="cardNumber"
+                name="card_number"
                 placeholder="5396 **** **** ****"
+                required
+                value={formData.card_number}
               />
             </FormGroup>
             <div className="details">
               <FormGroup className="mr-5">
                 <Label for="fullName">Exp. Date</Label>
-                <Input type="text" name="fullName" id="fullName" placeholder="10  /  21" />
+                <Input
+                  onChange={handleChange}
+                  pattern="\d\d/\d\d"
+                  type="text"
+                  name="exp_date"
+                  placeholder="10/21"
+                  required
+                  value={formData.exp_date}
+                />
               </FormGroup>
               <FormGroup>
                 <Label for="fullName">CVV</Label>
-                <Input type="text" name="fullName" id="fullName" placeholder="123" />
+                <Input
+                  onChange={handleChange}
+                  pattern="\d\d\d"
+                  type="text"
+                  name="cvv"
+                  placeholder="123"
+                  required
+                  value={formData.cvv}
+                />
               </FormGroup>
             </div>
           </div>
           <div className="buttons">
-            <AppButton buttonText="SAVE CARD & PAY" classname="all" />
-            <AppButton buttonText="PAY WITHOUT SAVING CARD" classname="all" />
+            <AppButton onClick={() => setPaymentMode('save_card')} type="submit" buttonText="SAVE CARD & PAY" classname="all" />
+            <AppButton onClick={() => setPaymentMode('no_save_card')} type="submit" buttonText="PAY WITHOUT SAVING CARD" classname="all" />
           </div>
         </div>
       </Form>
