c2471cc38107f86c6cc75499ce777ab5fa048b10
zainab.oyedeji <ololadeoselu@yahoo.com>
2021-05-05 12:42:31 +0100

    added scroll to some pages

--- src/components/HomeCategoryListComponent/index.jsx
+++ src/components/HomeCategoryListComponent/index.jsx
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import useCategoryStore from "../../stores/useCategoryStore";
 import clipText from "../../util/clipText";
 import LoadingTrigger from "../LoadingTrigger";
 import "./index.scss";
+import SliderButton from "../HorizontalSlider/SliderButton/SliderButton";
 
 export const HomeCategoryListComponent = () => {
   const { categories, categoriesLoading } = useCategoryStore();
+  const [showScrollLeftButton, setShowScrollLeftButton] = useState(false);
+  const [showScrollRightButton, setShowScrollRightButton] = useState(false);
+  const sliderContent = React.createRef();
+
+  useEffect(() => {
+    if (
+      sliderContent.current?.scrollWidth > sliderContent.current?.clientWidth
+    ) {
+      setShowScrollRightButton(true);
+    }
+  }, []);
+
+  const onScroll = (e) => {
+    const isScrolledLeft = sliderContent.current.scrollLeft <= 0;
+    setShowScrollLeftButton(!isScrolledLeft);
+
+    const isScrolledRight =
+      sliderContent.current?.scrollWidth -
+        sliderContent.current?.offsetWidth ===
+      sliderContent.current.scrollLeft;
+    setShowScrollRightButton(!isScrolledRight);
+  };
+
   return (
     <section className="categories-list-card">
-      <LoadingTrigger isLoading={categoriesLoading}>
-        {categories.map((category) => (
-          <CategoryItem key={category._id} category={category} />
-        ))}
-      </LoadingTrigger>
+      {showScrollRightButton ? (
+        <SliderButton
+          click={() => {
+            sliderContent.current.scrollBy(30, 0);
+          }}
+          position="right"
+        />
+      ) : null}
+      {showScrollLeftButton ? (
+        <SliderButton
+          click={() => {
+            sliderContent.current.scrollBy(-30, 0);
+          }}
+          position="left"
+        />
+      ) : null}
+      <div
+        className="categories-list-main"
+        ref={sliderContent}
+        onScroll={onScroll}
+      >
+        <LoadingTrigger isLoading={categoriesLoading}>
+          {categories.map((category) => (
+            <CategoryItem key={category._id} category={category} />
+          ))}
+        </LoadingTrigger>
+      </div>
     </section>
   );
 };
